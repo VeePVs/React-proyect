17b2dee1c9e0d2adcfb2082c8f695e5391f017ba
Andrea Pabuence <beepabuence@gmail.com>
2022-07-09 14:57:37 -0500

    Add react-icon

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import './TodoItem.css'
+import {IoIosCheckmarkCircle} from "react-icons/io"
+import {BsDashCircleFill} from "react-icons/bs"
 
 function TodoItem(props){
     return(
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon Icon-check--active'}`}
-            onClick={props.onComplete}
-            >
-                ✓
-            </span>
+            <IoIosCheckmarkCircle className={`Icon Icon-check ${props.completed && 'Icon Icon-check--active'}`}
+            onClick={props.onComplete} size={25}/>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
                 </p>
-            <span className="closeButton" onClick={props.onDelete}>X</span>
+            <BsDashCircleFill className="closeButton" onClick={props.onDelete} size={25}/>
         </li>
     );
 }
